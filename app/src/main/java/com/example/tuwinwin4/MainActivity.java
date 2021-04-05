95844f77a87cb4dd0feabd3d3d9892a354816a88
passanunmond <padsanan.par@dome.tu.ac.th>
2021-04-05 15:16:34 +0700

    TestMon

--- app/src/main/java/com/example/tuwinwin4/MainActivity.java
+++ app/src/main/java/com/example/tuwinwin4/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button button1 = (Button)findViewById(R.id.button100);
+        // Button button1 = (Button)findViewById(R.id.button100);
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
